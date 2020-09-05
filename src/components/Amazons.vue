8257197a1a859db70326b69ad622840f50c677ce
co <coer@kth.se>
2020-09-05 23:01:25 +0200

    Added mobile support and refoactored mouse event handling methods.

--- src/components/Amazons.vue
+++ src/components/Amazons.vue
@@ -116,6 +116,9 @@ const Amazons = defineComponent({
     this.canvas.onmousedown = this.onMouseDown;
     this.canvas.onmouseup = this.onMouseUp;
     this.canvas.onmousemove = this.onMouseMove;
+    this.canvas.addEventListener("touchstart", this.touchStart);
+    this.canvas.addEventListener("touchmove", this.touchMove);
+    this.canvas.addEventListener("touchend", this.touchEnd);
 
     this.draw();
   },
@@ -169,7 +172,7 @@ const Amazons = defineComponent({
     },
 
     drawLegalPositions(ctx: CanvasRenderingContext2D) {
-      ctx.globalAlpha = 0.3;
+      ctx.globalAlpha = 0.2;
       this.legalPositions.forEach((s) => {
         ctx.beginPath();
         ctx.arc(
@@ -180,7 +183,7 @@ const Amazons = defineComponent({
           2 * Math.PI,
           false
         );
-        ctx.fillStyle = "#ff6600";
+        ctx.fillStyle = "#00bbff";
         ctx.fill();
       });
       ctx.globalAlpha = 1;
@@ -333,33 +336,112 @@ const Amazons = defineComponent({
       );
     },
 
+    drawFloatingAmazon(
+      x: number,
+      y: number,
+      pieceSize: number,
+      isTouch: boolean
+    ) {
+      this.draw();
+      const ctx = this.canvas.getContext("2d")!;
+      const isMovingPieceWhite = isWhiteAmazon(
+        this.game.getSquareState(this.turnState.previousPosition!)
+      );
+      if (isTouch) {
+        const s = this.coordinateToSquare(x, y)
+        ctx.globalAlpha = 0.2;
+        ctx.beginPath();
+        ctx.arc(
+          s.x * this.squareSize + this.squareSize / 2,
+          s.y * this.squareSize + this.squareSize / 2,
+          this.squareSize,
+          0,
+          2 * Math.PI,
+          false
+        );
+        ctx.fillStyle = "#000000";
+        ctx.fill();
+        ctx.globalAlpha = 1;
+      }
+
+      ctx.drawImage(
+        isMovingPieceWhite ? this.whiteAmazonImage : this.blackAmazonImage,
+        x - pieceSize / 2,
+        isTouch ? y - pieceSize : y - pieceSize / 2,
+        pieceSize,
+        pieceSize
+      );
+    },
+
     onMouseMove(e: MouseEvent) {
       switch (this.turnState.phase) {
         case TurnPhase.AmazonMoving: {
           e.preventDefault();
           e.stopPropagation();
-          this.draw();
-          const ctx = this.canvas.getContext("2d")!;
           const mx = Math.round(e.clientX - this.boardOffset.x);
           const my = Math.round(e.clientY - this.boardOffset.y);
-          const isMovingPieceWhite = isWhiteAmazon(
-            this.game.getSquareState(this.turnState.previousPosition!)
-          );
-
-          ctx.drawImage(
-            isMovingPieceWhite ? this.whiteAmazonImage : this.blackAmazonImage,
-            mx - this.squareSize / 2,
-            my - this.squareSize / 2,
-            this.squareSize,
-            this.squareSize
-          );
+          this.drawFloatingAmazon(mx, my, this.squareSize, false);
           break;
         }
-        default:
-          break;
       }
     },
 
+    touchMove(e: TouchEvent) {
+      switch (this.turnState.phase) {
+        case TurnPhase.AmazonMoving: {
+          e.preventDefault();
+          e.stopPropagation();
+          const mx = Math.round(e.touches[0].clientX - this.boardOffset.x);
+          const my = Math.round(e.touches[0].clientY - this.boardOffset.y);
+          this.drawFloatingAmazon(mx, my, this.squareSize * 1.5, true);
+          break;
+        }
+      }
+    },
+
+    getTouchedPoint(tl: TouchList) {
+      const mx = Math.round(tl[0].clientX - this.boardOffset.x);
+      const my = Math.round(tl[0].clientY - this.boardOffset.y);
+      return this.coordinateToSquare(mx, my);
+    },
+
+    getPointAtMouse(e: MouseEvent) {
+      const mx = Math.round(e.clientX - this.boardOffset.x);
+      const my = Math.round(e.clientY - this.boardOffset.y);
+      return this.coordinateToSquare(mx, my);
+    },
+
+    touchStart(e: TouchEvent) {
+      switch (this.turnState.phase) {
+        case TurnPhase.Start: {
+          e.preventDefault();
+          e.stopPropagation();
+
+          this.tryStartMoveAmazon(this.getTouchedPoint(e.touches));
+
+          this.draw();
+        }
+      }
+    },
+
+    tryStartMoveAmazon(boardPosition: Point) {
+      if (this.game.turn == Color.White) {
+        if (!isWhiteAmazon(this.game.getSquareState(boardPosition))) {
+          return;
+        }
+      }
+
+      if (this.game.turn == Color.Black) {
+        if (!isBlackAmazon(this.game.getSquareState(boardPosition))) {
+          return;
+        }
+      }
+
+      this.turnState.startMovingAmazon(boardPosition);
+      this.legalPositions = this.game.getPossibleQueenMovesFromPoint(
+        boardPosition
+      );
+    },
     onMouseDown(e: MouseEvent) {
       switch (this.turnState.phase) {
         case TurnPhase.Start: {
@@ -368,52 +450,21 @@ const Amazons = defineComponent({
 
           const mx = Math.round(e.clientX - this.boardOffset.x);
           const my = Math.round(e.clientY - this.boardOffset.y);
-          const boardPosition = this.coordinateToSquare(mx, my);
-
-          if (this.game.turn == Color.White) {
-            if (!isWhiteAmazon(this.game.getSquareState(boardPosition))) {
-              return;
-            }
-          }
-
-          if (this.game.turn == Color.Black) {
-            if (!isBlackAmazon(this.game.getSquareState(boardPosition))) {
-              return;
-            }
-          }
-
-          this.turnState.startMovingAmazon(boardPosition);
-          this.legalPositions = this.game.getPossibleQueenMovesFromPoint(
-            boardPosition
-          );
+          this.tryStartMoveAmazon(this.coordinateToSquare(mx, my));
           this.draw();
           break;
         }
-
-        default:
-          break;
       }
     },
-    onMouseUp(e: MouseEvent) {
+    touchEnd(e: TouchEvent) {
       switch (this.turnState.phase) {
         case TurnPhase.AmazonMoving: {
           e.preventDefault();
           e.stopPropagation();
 
-          const mx = Math.round(e.clientX - this.boardOffset.x);
-          const my = Math.round(e.clientY - this.boardOffset.y);
-          const newPosition = this.coordinateToSquare(mx, my);
-
-          const oldPosition = this.turnState.previousPosition!;
-          if (this.game.isPointQueenMoveAway(oldPosition, newPosition)) {
-            this.turnState.prepareToShoot(newPosition);
-            this.legalPositions = this.game.getPossibleQueenMovesFromPointIgnoreObstacleAt(
-              newPosition,
-              oldPosition
-            );
-          } else {
-            this.resetTurnState();
-          }
+          this.tryMakeFirstPartOfMoveByMovingAmazon(
+            this.getTouchedPoint(e.changedTouches)
+          );
           this.draw();
           break;
         }
@@ -422,60 +473,69 @@ const Amazons = defineComponent({
           e.preventDefault();
           e.stopPropagation();
 
-          const mx = Math.round(e.clientX - this.boardOffset.x);
-          const my = Math.round(e.clientY - this.boardOffset.y);
-          const arrowHitPosition = this.coordinateToSquare(mx, my);
-          const previousPosition = this.turnState.previousPosition!;
-          if (
-            !(
-              this.game.canShootAtPoint(
-                this.turnState.positionToShootFrom!,
-                arrowHitPosition,
-                this.turnState.previousPosition!
-              ) ||
-              (previousPosition.x == arrowHitPosition.x &&
-                previousPosition!.y == arrowHitPosition.y)
-            )
-          ) {
-            this.resetTurnState();
-            this.draw();
-            return;
-          }
-
-          this.game.makeMove(
-            this.turnState.previousPosition!,
-            this.turnState.positionToShootFrom!,
-            arrowHitPosition
+          this.tryFinishMoveShootingAtPosition(
+            this.getTouchedPoint(e.changedTouches)
           );
-          this.resetTurnState();
           this.draw();
           break;
         }
-        default:
-          break;
       }
     },
-  },
-  computed: {
-    viewBox(props: { size: number }): string {
-      return `0 0 ${props.size * 20} ${props.size * 20} `;
-    },
-    squares() {
-      const result = [];
 
-      let x;
-      let y;
-      let isWhite = true;
-      for (y = 0; y < this.boardSize; y++) {
-        for (x = 0; x < this.boardSize; x++) {
-          result.push({ x: x, y: y, isWhite: isWhite });
-          isWhite = !isWhite;
-        }
-        isWhite = !isWhite;
+    tryFinishMoveShootingAtPosition(arrowHitPosition: Point) {
+      const previousPosition = this.turnState.previousPosition!;
+      if (
+        this.game.canShootAtPoint(
+          this.turnState.positionToShootFrom!,
+          arrowHitPosition,
+          this.turnState.previousPosition!
+        )
+      ) {
+        this.game.makeMove(
+          this.turnState.previousPosition!,
+          this.turnState.positionToShootFrom!,
+          arrowHitPosition
+        );
       }
-      return result;
+      this.resetTurnState();
+    },
+
+    tryMakeFirstPartOfMoveByMovingAmazon(newPosition: Point) {
+      const oldPosition = this.turnState.previousPosition!;
+      if (this.game.isPointQueenMoveAway(oldPosition, newPosition)) {
+        this.turnState.prepareToShoot(newPosition);
+        this.legalPositions = this.game.getPossibleQueenMovesFromPointIgnoreObstacleAt(
+          newPosition,
+          oldPosition
+        );
+      } else {
+        this.resetTurnState();
+      }
+    },
+
+    onMouseUp(e: MouseEvent) {
+      switch (this.turnState.phase) {
+        case TurnPhase.AmazonMoving: {
+          e.preventDefault();
+          e.stopPropagation();
+
+          this.tryMakeFirstPartOfMoveByMovingAmazon(this.getPointAtMouse(e));
+          this.draw();
+          break;
+        }
+
+        case TurnPhase.WaitingToShoot: {
+          e.preventDefault();
+          e.stopPropagation();
+
+          this.tryFinishMoveShootingAtPosition(this.getPointAtMouse(e));
+          this.draw();
+          break;
+        }
+      }
     },
   },
+  computed: {},
 });
 export default Amazons;
 </script>
